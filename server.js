08e5528fb199fc6943791d521a43da9c208abdaf
jzwei <jzweibel@gmail.com>
2020-09-02 10:22:53 -0400

    update

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require("express");
+const server = express();
+const port = 4000;
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.get("/json", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
+server.get("/items", (req, res) => {
+  res.json({
+    items: [
+      { id: 1, name: "banana" },
+      { id: 2, name: "apple" },
+    ],
+  });
+});
+
+server.get("/info", (req, res) => {
+  res.sendFile(__dirname + "/info.html");
+});
